12d540fd9eb05d1c227d51fc1d07660e08b456bd
jebuske <hannes.hardeman@gmail.com>
2018-01-26 15:21:31 +0100

    contract made simpler by letting player 1 send his move upon creation of the game and therefor player 2 can send his move in the open

--- contracts/RockPaperScissor.sol
+++ contracts/RockPaperScissor.sol
@@ -3,31 +3,32 @@ pragma solidity ^0.4.4;
 import "./Stopable.sol";
 
 contract RockPaperScissor is Stopable {
+uint gameid;
 
   struct Game {
     address player1;
     address player2;
     address winner;
-    uint deadlineHashedMove;
+    uint deadlinePlayer2;
     uint deadlineRevealMove;
     uint stake;
     bytes32 player1HashedMove;
     bytes32 player2HashedMove;
     uint player1Move;
     uint player2Move;
+    bool winnerDetermined;
   }
 
 mapping (uint => Game) games;
-mapping (uint => bool) usedIds;
 mapping (bytes32 => bool) usedPasswords;
 mapping (address => uint) withdrawals;
-mapping (uint => bool) winnerDeterimined;
 
-event LogGameCreation(address player1, address player2, uint stake, uint deadlineHashedMove, uint deadlineRevealMove);
-event LogHashedPlayermoveSent(address player, bytes32 hashedPlayerMove);
+event LogGameCreation(address player1, address player2, uint stake, uint deadlinePlayer2, uint deadlineRevealMove, uint gameid);
+event LogPlayer2MoveSent(address player, uint playerMove);
 event LogRevealSent(address player, uint playermove);
 event LogWinner(address winner);
 event LogWithdrawal(uint amount, address withdrawer);
+event LogHash(bytes32);
 
   function RockPaperScissor() {
       running = true;
@@ -35,92 +36,82 @@ event LogWithdrawal(uint amount, address withdrawer);
   }
 
 //start a new game with 2 players and send the stake with it
-  function startNewGame (uint _id, address _player2, uint _deadlineHashedMove, uint _deadlineRevealMove)
+  function startNewGame (address _player2, bytes32 _player1HashedMove, uint _deadlinePlayer2, uint _deadlineRevealMove)
   onlyIfRunning
-  public
   payable
-  {
-    require(!usedIds[_id]);
+  public
+  { 
+    uint id = gameid+1;
     require(_player2 != msg.sender);
     require(_player2 != address(0));
     require(msg.value > 0);
-    require(_deadlineHashedMove > 0);
-    require(_deadlineRevealMove > _deadlineHashedMove+2);
-    
-    games[_id].player1 = msg.sender;
-    games[_id].player2 = _player2;
-    games[_id].deadlineHashedMove = _deadlineHashedMove;
-    games[_id].stake = msg.value;
-    usedIds[_id] = true;
-    LogGameCreation(msg.sender, _player2, msg.value, _deadlineHashedMove, _deadlineRevealMove);
+    require(_deadlinePlayer2 > 0);
+    require(_deadlineRevealMove > _deadlinePlayer2+2);
+    games[id].player1 = msg.sender;
+    games[id].player2 = _player2;
+    games[id].deadlinePlayer2 = _deadlinePlayer2;
+    games[id].stake = msg.value;
+    games[id].player1HashedMove = _player1HashedMove;
+    LogGameCreation(msg.sender, _player2, msg.value, _deadlinePlayer2, _deadlineRevealMove, id);
   }
 
 //both players should send in their hashed playermove before the deadline. PlayerMoveHash should contain a uint between 1 and 3 and a secret password
-  function sendHashedPayerMove(uint _gameid, bytes32 _playerMoveHash)
+  function player2SendMove(uint _gameid, uint _player2Move)
+  payable
   {
-    require (now <= games[_gameid].deadlineHashedMove);
-    require (!usedPasswords[_playerMoveHash]);
-    if (msg.sender == games[_gameid].player1){
-      require(games[_gameid].player1HashedMove == 0);
-      games[_gameid].player1HashedMove = _playerMoveHash;
-      usedPasswords[_playerMoveHash] = true;
+      //send 1) Rock   2) Paper   3)Scissor
+    //require (now <= games[_gameid].deadlinePlayer2);
+    require (msg.sender == games[_gameid].player2);
+    require (games[_gameid].player2Move == 0);
+    require (msg.value == games[_gameid].stake);
+    games[_gameid].player2Move = _player2Move;
+    games[_gameid].stake += msg.value;
+    LogPlayer2MoveSent(msg.sender, _player2Move);
+  }
+
+  function checkPlayer2Move(uint _gameid){
+   // require (now > games[_gameid].deadlinePlayer2);
+    address player1 = games[_gameid].player1;
+    if (games[_gameid].player2Move == 0){
+        withdrawals[player1] = games[_gameid].stake;
     }
-    if (msg.sender == games[_gameid].player2){
-      require(games[_gameid].player2HashedMove == 0);
-      games[_gameid].player1HashedMove = _playerMoveHash;
-      usedPasswords[_playerMoveHash] = true;
-      
-    }
-    LogHashedPlayermoveSent(msg.sender, _playerMoveHash);
   }
 
   //after deadline: players should send in their revealed playermove before the second deadline. If they don't send it in, the playermove value will equal to 0 and they will loose.
-  function sendPlayerMove(uint _gameid, uint _playermove, bytes32 _password)
+  function sendPlayer1Move(uint _gameid, uint _playermove, bytes32 _password)
   {
-    require (now > games[_gameid].deadlineHashedMove);
-    require (now <= games[_gameid].deadlineRevealMove);
+    //require (now > games[_gameid].deadlinePlayer2);
+    //require (now <= games[_gameid].deadlineRevealMove);
     bytes32 hashedPlayerMove = keccak256(_playermove, _password);
-    if (msg.sender == games[_gameid].player1){
-      require(hashedPlayerMove == games[_gameid].player1HashedMove);
-      games[_gameid].player1Move = _playermove;
-    }
-     if (msg.sender == games[_gameid].player2){
-     require(hashedPlayerMove == games[_gameid].player1HashedMove);
-      games[_gameid].player2Move = _playermove;
-    }
-    LogRevealSent(msg.sender, _playermove);
+    LogHash(hashedPlayerMove);
+    require(hashedPlayerMove == games[_gameid].player1HashedMove);
+    games[_gameid].player1Move = _playermove;
+    LogRevealSent(msg.sender, _playermove);  
   }
 
   //determine winner
   function determineWinner(uint _gameid)
   {
-    require (!winnerDeterimined[_gameid]);
-    require (now > games[_gameid].deadlineRevealMove);
-  address player1 = games[_gameid].player1;
-  address player2 = games[_gameid].player2;
-  if (games[_gameid].player1Move == 0 && games[_gameid].player2Move == 0){
-    withdrawals[player1] = games[_gameid].stake/2;
-    withdrawals[player2] = games[_gameid].stake/2;
-  }
-  if (games[_gameid].player1Move == 0){
-    withdrawals[player2] = games[_gameid].stake/2;
-  }
-  if (games[_gameid].player2Move == 0){
-    withdrawals[player1] = games[_gameid].stake/2;
-  }
+    require (!games[_gameid].winnerDetermined);
+    //require (now > games[_gameid].deadlineRevealMove);
+    address player1 = games[_gameid].player1;
+    address player2 = games[_gameid].player2;
+  
   if (games[_gameid].player1Move%3+1 == games[_gameid].player2Move){
    withdrawals[player2] = games[_gameid].stake;
-   LogWinner(player2);
+   games[_gameid].winner = player2;
+   LogWinner(games[_gameid].winner);
   }
-  if (games[_gameid].player2Move%3+1 == games[_gameid].player1Move){
+  else if (games[_gameid].player2Move%3+1 == games[_gameid].player1Move){
     withdrawals[player1] = games[_gameid].stake;
-    LogWinner(player1);
+    games[_gameid].winner = player1;
+    LogWinner(games[_gameid].winner);
   }
   else {
     withdrawals[player1] = games[_gameid].stake/2;
     withdrawals[player2] = games[_gameid].stake/2;
   }
-  winnerDeterimined[_gameid]=true;
+  games[_gameid].winnerDetermined=true;
   }
   
   function withdraw () returns (bool) {
